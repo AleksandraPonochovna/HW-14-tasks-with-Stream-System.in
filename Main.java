25278db0a77fb46a57c3d8f2892474128c059444
Aleksandra Ponochovna <47608387+AleksandraPonochovna@users.noreply.github.com>
2019-06-23 15:24:43 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
 package com.company;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.Map;
 import java.util.Scanner;
 import java.util.stream.Collectors;
@@ -9,10 +10,20 @@ public class Main {
 
     public static void main(String[] args) {
         Main main = new Main();
-        main.method();
+        main.readOrProcessing();
     }
 
-    public void method() {
+    private static Comparator<Map.Entry<String, Long>> comparatorForAlphabet() {
+        return (o1, o2) -> {
+            if (o1.getValue().equals(o2.getValue())) {
+                return o1.getKey().compareTo(o2.getKey());
+            } else {
+                return o2.getValue().compareTo(o1.getValue());
+            }
+        };
+    }
+
+    public void readOrProcessing() {
         Scanner scn = new Scanner(System.in, "UTF-8");
         String[] strings = scn.nextLine().split("[\\p{Punct}\\s]+");
         Arrays.stream(strings)
@@ -20,16 +31,9 @@ public class Main {
                 .collect(Collectors.groupingByConcurrent(word -> word, Collectors.counting()))
                 .entrySet()
                 .stream()
-                .sorted((o1, o2) -> {
-                    if (o1.getValue() == o2.getValue()) {
-                        return o1.getKey().compareTo(o2.getKey());
-                    } else {
-                        return o2.getValue().compareTo(o1.getValue());
-                    }
-                })
+                .sorted(comparatorForAlphabet())
                 .limit(10)
                 .map(Map.Entry::getKey)
                 .forEach(System.out::println);
     }
 }
-
