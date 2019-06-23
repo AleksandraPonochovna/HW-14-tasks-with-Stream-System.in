3e587c357c5f031eaee06ea6dcd59b8e7f8ad8e7
Aleksandra Ponochovna <47608387+AleksandraPonochovna@users.noreply.github.com>
2019-06-23 15:14:20 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,1 +1,35 @@
+package com.company;
+
+import java.util.Arrays;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Main main = new Main();
+        main.method();
+    }
+
+    public void method() {
+        Scanner scn = new Scanner(System.in, "UTF-8");
+        String[] strings = scn.nextLine().split("[\\p{Punct}\\s]+");
+        Arrays.stream(strings)
+                .map(String::toLowerCase)
+                .collect(Collectors.groupingByConcurrent(word -> word, Collectors.counting()))
+                .entrySet()
+                .stream()
+                .sorted((o1, o2) -> {
+                    if (o1.getValue() == o2.getValue()) {
+                        return o1.getKey().compareTo(o2.getKey());
+                    } else {
+                        return o2.getValue().compareTo(o1.getValue());
+                    }
+                })
+                .limit(10)
+                .map(Map.Entry::getKey)
+                .forEach(System.out::println);
+    }
+}
 
